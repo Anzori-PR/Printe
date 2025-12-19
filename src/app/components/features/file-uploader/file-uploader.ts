498650bce447089d5d3aec9a14044cc2a1dd958a
Anzori-PR <saxelashvilianzori@gmail.com>
2025-12-19 15:50:03 +0400

    feat: add header and footer components to app layout
    feat: create checkout form component with basic structure
    
    feat: create file uploader component with basic structure
    
    feat: create product selector component with basic structure
    
    feat: create order wizard component with basic structure
    
    feat: create success page component with basic structure
    
    feat: implement file upload service for handling file uploads
    
    feat: implement order state service for managing order state
    
    style: add global container styles for responsive design

---
+++ src/app/components/features/file-uploader/file-uploader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-uploader',
+  imports: [],
+  templateUrl: './file-uploader.html',
+  styleUrl: './file-uploader.css',
+})
+export class FileUploader {
+
+}
---
+++ src/app/pages/order-wizard/order-wizard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-wizard',
+  imports: [],
+  templateUrl: './order-wizard.html',
+  styleUrl: './order-wizard.css',
+})
+export class OrderWizard {
+
+}
