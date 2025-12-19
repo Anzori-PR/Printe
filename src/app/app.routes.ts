7f3593838df4607293b6083fd679ec58c02a90aa
Anzori-PR <saxelashvilianzori@gmail.com>
2025-12-20 02:13:49 +0400

    feat: integrate FontAwesome icons and enhance app layout with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.Home)},
+    {path: 'products', loadComponent: () => import('./components/features/product-selector/product-selector').then(m => m.ProductSelector)},
+    {path: '**', redirectTo: 'home'},
+];
--- src/app/components/layout/header/header.ts
+++ src/app/components/layout/header/header.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
+  faCart = faCartShopping;
+  faSearch = faSearch;
 
+  constructor(private router: Router) {}
+  ngOnInit(): void {}
+
+
+  homePage() {
+    this.router.navigate(['/home']);
+  }
 }
